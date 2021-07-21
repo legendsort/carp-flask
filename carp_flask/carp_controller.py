c62cd470e5dcbd027cff48f307b3be7577cb3609
Sortoite <zachatatem@gmail.com>
2021-07-21 05:03:22 -0500

    Upgraded the carp-python-api.

--- carp_flask/carp_controller.py
+++ carp_flask/carp_controller.py
@@ -61,7 +61,6 @@ def login():
     Endpoint: [login]
     :return: The login tokens.
     """
-    print(request.form.get('username'))
     auth_response = account.login(BASE_URL["production"], request)
     return Response(json.dumps(auth_response), mimetype='application/json')
 
